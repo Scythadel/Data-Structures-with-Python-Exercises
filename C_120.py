f60cebc885c956575c84c7221079595ac43c5690
Scythadel <pradhamp@hotmail.com>
2024-07-01 21:58:46 +0530

    Tried to incorporate while condition

--- C_120.py
+++ C_120.py
@@ -1,10 +1,12 @@
 from random import randint
 def shuffle(data):
-    for i in range(len(data) - 1):
+    k = 0
+    for i in data:
         random_range = randint(0, len(data) - 1)
-        if i == random_range:
+        while k == random_range:
             random_range = randint(0, len(data) - 1)
-        data[i] = data[random_range]
+        data[k] = data[random_range]
+        k = k + 1
     print(data)
 
 shuffle([1,2,3,4])
