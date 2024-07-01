002cf4b4924ec5eec4212fe9adfa55199041e246
Scythadel <pradhamp@hotmail.com>
2024-07-01 22:46:01 +0530

    Added a try except block

--- C_121.py
+++ C_121.py
@@ -1,7 +1,8 @@
 b = []
-while not EOFError:
-    a = str(input("Enter standard input: "))
-    b.append(a)
-
-b.reverse()
-print(b)
+while 0 == 0:
+    try:
+        a = str(input("Enter standard input: "))
+        b.append(a)
+    except EOFError:
+        b.reverse()
+        print(b)
